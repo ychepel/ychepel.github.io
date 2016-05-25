7579da89f3f789887e471dabd4a9d3a966ffeb7f
Yuriy Chepel <yuriy.chepel@gmail.com>
2016-05-25 15:35:25 +0300

    change some logic

--- main.js
+++ main.js
@@ -32,9 +32,14 @@ function Animal() {
 	this.direction = [];
 };
 Animal.prototype.render = function() {
-	context.font = this.size + fontSuffix;
+	context.beginPath();
+	context.arc(this.x, this.y, this.size , 0, 2 * Math.PI);
+	context.stroke();
 	context.fillStyle = this.color;
-	context.fillText(this.body, this.x, this.y);
+	context.fill();
+	context.fillStyle = 'white';
+	context.font = this.size + fontSuffix;
+	context.fillText(this.body, this.x-this.size/2 , this.y + this.size/2);
 };
 
 function Cat() {Animal.call(this)};
@@ -62,24 +67,24 @@ Rabbit.prototype.body = 'R';
 Rabbit.prototype.color = "gray";
 Rabbit.prototype.stepToGrowth = 25;
 Rabbit.prototype.maxSize = 40;
-Rabbit.prototype.sizeToClone = 16;
+Rabbit.prototype.sizeToClone = 18;
 
 function Monster() {Animal.call(this)};
 Monster.prototype = Object.create(Animal.prototype);
 Monster.prototype.constructor = Monster;
 Monster.prototype.body = 'M';
 Monster.prototype.color = "red";
-Monster.prototype.stepToGrowth = 35;
-Monster.prototype.maxSize = 30;
-Monster.prototype.sizeToClone = 31;
+Monster.prototype.stepToGrowth = 30;
+Monster.prototype.maxSize = 50;
+Monster.prototype.sizeToClone = 51;
 
 
 function init(animal, count, posX, posY) {
 	for (var i = 0; i < count; i++) {
 		var currentAnimal = new animal();
-		currentAnimal.x = (posX > 0 ? posX : getRandomInt(0, maxX));
 		currentAnimal.size = 15;
-		currentAnimal.y = (posY > 0 ? posY : getRandomInt(currentAnimal.size, maxY));
+		currentAnimal.x = (posX != undefined ? posX : getRandomInt(currentAnimal.size, maxX - currentAnimal.size));
+		currentAnimal.y = (posY != undefined ? posY : getRandomInt(currentAnimal.size, maxY - currentAnimal.size));
 		currentAnimal.direction.push(horisontalDirections[getRandomInt(0,2)]);
 		currentAnimal.direction.push(verticalDirections[getRandomInt(0,2)]);
 		population.push(currentAnimal);
@@ -106,37 +111,41 @@ function draw() {
 function move() {
 	for(var animalNum in population) {
 		var animal = population[animalNum];
-		switch (animal.direction[0]) {
-			case 'L': animal.x -= animal.size/2; 
-				if(animal.x < 0) {
-					animal.direction[0] = oppositeDirection(animal.direction[0]);
-					animal.x = 0;
-				}
-				break;
-			case 'R': animal.x += animal.size/2; 
-				if(animal.x + animal.size > maxX) {
-					animal.direction[0] = oppositeDirection(animal.direction[0]);
-					animal.x = maxX - animal.size;
-				}
-				break;
-		}
-		switch (animal.direction[1]) {
-			case 'U': animal.y -= animal.size/2; 
-				if(animal.y - animal.size < 0) {
-					animal.direction[1] = oppositeDirection(animal.direction[1]);
-					animal.y = animal.size;
-				}
-				break;
-			case 'D': animal.y += animal.size/2; 
-				if(animal.y > maxY) {
-					animal.direction[1] = oppositeDirection(animal.direction[1]);
-					animal.y = maxY - animal.size;
-				}
-				break;
-		}
+		doStep(animal);
 		animal.step++;
 		growthAnimal.call(animal);
 		checkContact(animal);
+	}
+}
+
+function doStep(animal) {
+	switch (animal.direction[0]) {
+		case 'L': animal.x -= animal.size/2; 
+			if(animal.x - animal.size < 0) {
+				animal.direction[0] = oppositeDirection(animal.direction[0]);
+				animal.x = animal.size;
+			}
+			break;
+		case 'R': animal.x += animal.size/2; 
+			if(animal.x + animal.size > maxX) {
+				animal.direction[0] = oppositeDirection(animal.direction[0]);
+				animal.x = maxX - animal.size;
+			}
+			break;
+	}
+	switch (animal.direction[1]) {
+		case 'U': animal.y -= animal.size/2; 
+			if(animal.y - animal.size < 0) {
+				animal.direction[1] = oppositeDirection(animal.direction[1]);
+				animal.y = animal.size;
+			}
+			break;
+		case 'D': animal.y += animal.size/2; 
+			if(animal.y + animal.size > maxY) {
+				animal.direction[1] = oppositeDirection(animal.direction[1]);
+				animal.y = maxY - animal.size;
+			}
+			break;
 	}
 }
 
@@ -144,10 +153,12 @@ function checkContact(currentAnimal) {
 	for(var animalNum in population) {
 		var animal = population[animalNum];
 		if(animal != currentAnimal) {
-			if(animal.x <= currentAnimal.x && animal.x + animal.size >= currentAnimal.x) {
-				if(animal.y <= currentAnimal.y && animal.y + animal.size >= currentAnimal.y) {
-					contact(currentAnimal, animal);
-				}
+			if(animal.x - animal.size <= currentAnimal.x 
+				&& animal.x + animal.size >= currentAnimal.x) {
+					if(animal.y - animal.size <= currentAnimal.y 
+						&& animal.y + animal.size >= currentAnimal.y) {
+							contact(currentAnimal, animal);
+					}
 			}
 		}
 	}
@@ -157,16 +168,23 @@ function contact(animal1, animal2) {
 	if(animal1.constructor.name === animal2.constructor.name) {
 		if(animal1.size >= animal1.sizeToClone) {
 			if(animal2.size >= animal2.sizeToClone) {
-				init(animal1.constructor, 1, animal1.x, animal1.y);
+				init(animal1.constructor, 1, animal1.x + 20, animal1.y + 20);
+				if(animal1.direction[0] === '' && animal1.direction[1] === '') {
+					removeAnimal(animal1);
+				}
+				if(animal2.direction[0] === '' && animal2.direction[1] === '') {
+					removeAnimal(animal2);
+				}
+				return;
 			}
 		}
 	}
 
 	if(animal1.constructor.name === 'Rabbit' && animal2.constructor.name != 'Rabbit') {
-		init(Monster, 1, animal2.x, animal2.y);
+		init(Monster, 1, animal2.x + 20, animal2.y + 20);
 	}
 	if(animal1.constructor.name != 'Rabbit' && animal2.constructor.name === 'Rabbit') {
-		init(Monster, 1, animal1.x, animal1.y);
+		init(Monster, 1, animal1.x + 20, animal1.y + 20);
 	}
 
 
@@ -198,10 +216,10 @@ function animation() {
 		context.clearRect(0, 0, maxX, maxY);
 		draw();
 		move();		
-	}, 500);
+	}, 100);
 };
 
-init(Cat, 6);
-init(Dog, 6);
-init(Rabbit, 3);
+init(Cat, 5);
+init(Dog, 5);
+init(Rabbit, 5);
 animation();
